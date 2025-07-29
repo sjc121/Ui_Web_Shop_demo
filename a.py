69dafa4b141319b23952e608431553972a53c643
sjc121 <13283306673@163.com>
2025-07-29 11:15:34 +0800

    第二次提交

--- a.py
+++
@@ -1,8 +0,0 @@
-import sys
-import os
-
-print('当前 Python 解释器路径：' + sys.executable)
-
-# 当前 Python 解释器路径：D:\pythonprojects\pythonProject_http\.venv\Scripts\python.exe
-
-print('当前 Python 解释器目录：' + os.path.dirname(sys.executable))
