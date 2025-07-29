d5d14a82e48e005b385d847a3cf40c7684f57ebd
sjc121 <13283306673@163.com>
2025-07-29 11:24:54 +0800

    删除test_login_OK_03

--- test_login.py
+++ test_login.py
@@ -40,20 +40,3 @@ class TestLogin:
         sleep(1)
         text = driver.find_element(By.XPATH, "//div[@class='user_error']").text
         assert text == "用户名错误"
-
-    def test_login_OK_03(self):
-        # 打开浏览器，和基础设置
-        driver = webdriver.Edge()
-        driver.get("http://127.0.0.1:8000")
-        driver.maximize_window()  # 打开浏览器窗口最大化
-        driver.implicitly_wait(10)  # 隐式等待10秒
-        driver.find_element(By.XPATH, "/html/body/div[1]/div/div[2]/div[1]/a[1]").click()
-        sleep(1)
-        driver.find_element(By.XPATH, "/html/body/div[2]/div/div[3]/div[2]/form/input[2]").send_keys("test")
-        sleep(1)
-        driver.find_element(By.XPATH, "/html/body/div[2]/div/div[3]/div[2]/form/input[3]").send_keys("test123")
-        sleep(1)
-        driver.find_element(By.XPATH, "/html/body/div[2]/div/div[3]/div[2]/form/input[4]").click()
-        sleep(1)
-        text = driver.find_element(By.XPATH, "//div[@class='user_error']").text
-        assert text == "用户名错误"
